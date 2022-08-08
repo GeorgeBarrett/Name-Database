3fe18d5803d3c1e3f95a357081373678c718e42b
George Barrett <georgeaustinbarrett@gmail.com>
2022-08-08 17:53:04 +0100

    end of the first day

--- client/script.js
+++ client/script.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:5000/getAll')
+    .then(response => response.json())
+    
+    // if I pass 'data' into the object and data into the parenthases then data table disappears
+    .then(data => console.log(data))
     loadHtmlTable([])
 });
 
 function loadHtmlTable(data) {
 
     const table = document.querySelector('table tbody');
-    let tableHtml = '';
 
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No data</td></tr>";
